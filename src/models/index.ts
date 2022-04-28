ccc066f9a794305f437c10892207b88371b4d585
Connor Hogan <cdhogan084@gmail.com>
2022-04-27 23:57:30 -0400

    update models/index.ts to remove Game

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,9 @@
 import Player from './player.model';
-import Game, { GameType } from './game.model';
 import PlayLogEntry from './play-log-entry.model';
 import Group, { GroupPrivacyType } from './group.model';
 
 export {
   Player,
-  Game,
-  GameType,
   PlayLogEntry,
   Group,
   GroupPrivacyType,
