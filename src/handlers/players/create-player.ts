2ced4e40a0c70d4d3fd998cbd15f6c1c6d9ad602
Connor Hogan <cdhogan084@gmail.com>
2022-05-27 17:01:15 -0400

    adding players resource and associated infrastructure

---
+++ src/handlers/players/create-player.ts
@@ -0,0 +1,48 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { nanoid } from 'nanoid';
+import { IDbService } from '../../interfaces';
+import { diContainer } from '../../inversify.config';
+import { Player } from '../../models';
+import { InjectableTypes } from '../../types';
+import * as logger from 'lambda-log';
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.options.meta = {
+    environment: process.env.APP_ENVIRONMENT
+  };
+  logger.info('Incoming request', event?.requestContext || '');
+  const dbService = diContainer.get<IDbService>(InjectableTypes.DynamoDbService);
+  let response: APIGatewayProxyResult;
+
+  try {
+    const requestBody = event?.body || '';
+    const player = JSON.parse(requestBody) as Player;
+    const user = event?.requestContext?.authorizer?.jwt?.claims?.email || '';
+
+    // TODO: Add validation
+    if (!player) {
+      throw new Error('Missing Player in POST body.');
+    }
+
+    player.id = nanoid();
+    player.createdDate = player.lastUpdatedDate = Date.now();
+    player.createdBy = player.lastUpdatedBy = player.user = user;
+
+    const table = process.env.PLAYERS_TABLE || '';
+
+    await dbService.put<Player>(table, player);
+
+    response = {
+      statusCode: 200,
+      body: JSON.stringify(player)
+    };
+
+  } catch (err) {
+    logger.error(err as Error);
+    response = {
+      statusCode: 500,
+      body: 'An error occured while creating Player.'
+    }
+  }
+  return response;
+}
---
+++ src/handlers/players/update-player.ts
@@ -0,0 +1,52 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { IDbService } from '../../interfaces';
+import { diContainer } from '../../inversify.config';
+import { Player } from '../../models';
+import { InjectableTypes } from '../../types';
+import * as logger from 'lambda-log';
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.options.meta = {
+    environment: process.env.APP_ENVIRONMENT
+  };
+  logger.info('Incoming request', event?.requestContext || '');
+  const dbService = diContainer.get<IDbService>(InjectableTypes.DynamoDbService);
+  let response: APIGatewayProxyResult;
+
+  try {
+    const id = event?.pathParameters?.id || false;
+    const user = event?.requestContext?.authorizer?.jwt?.claims?.email || '';
+
+    if (!id) {
+      throw new Error('Missing id in path');
+    }
+
+    const requestBody = event?.body || '';
+    const player = JSON.parse(requestBody) as Player;
+
+    // TODO: Add validation
+    if (!player) {
+      throw new Error('Missing Player in PUT body');
+    }
+
+    player.lastUpdatedDate = Date.now();
+    player.lastUpdatedBy = user;
+
+    const table = process.env.GROUPS_TABLE || '';
+
+    const updatedPlayer = await dbService.update<Player>(table, id, player);
+
+    response = {
+      statusCode: 200,
+      body: JSON.stringify(updatedPlayer)
+    };
+
+  } catch (err) {
+    logger.error(err as Error);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify(err)
+    }
+  }
+  return response;
+}
